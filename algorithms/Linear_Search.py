2a3d754ef6280e34bb18e6fb2cc37b7a2785faa8
MD. Mehedi Hasan <emonmehedi61@gmail.com>
2018-05-10 23:19:32 +0600

    restructured the repository

---
+++ algorithms/Linear_Search.py
@@ -0,0 +1,17 @@
+# Linear Search Algorithm with Python3
+# Programmed by MD. Mehedi Hasan
+
+my_list = [1, 2, 3, 4, 5]
+
+search_item = int(input("enter a number: "))
+
+if search_item in my_list:
+
+	get_index = my_list.index(search_item)   # index number of the search_item
+	
+	print(str(search_item) + " is found at index number " + str(get_index) + " .")
+
+else:
+	
+	print(str(search_item) + " is not on the list.")
+
